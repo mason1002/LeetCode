c2828a5fa8c5a2ef45a04e08ab0cc9d4f9731e26
Mason <ms.catmissyou@gmail.com>
2018-05-23 01:59:56 +1000

    Flatten binary tree 2 linkedlist

---
+++ src/FlattenBinaryTree2LinkedList/TreeNode.java
@@ -0,0 +1,24 @@
+package FlattenBinaryTree2LinkedList;
+/*
+     Author:Mason
+     2018/5/23 1:33
+         /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+*/
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
